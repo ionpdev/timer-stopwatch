e7386e415d1ca885ced2a5f47c01130cdfdfb9ea
johny2433 <johny.codmw3@gmail.com>
2020-10-10 21:01:14 +0300

    second commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const durationInput = document.querySelector('#duration')
+const startButton = document.querySelector('#start')
+const pauseButton = document.querySelector('#pause')
+const circle = document.querySelector('circle')
+
+const perimeter = circle.getAttribute('r') * 2 * Math.PI
+circle.setAttribute('stroke-dasharray', perimeter)
+
+let duration
+
+const timer = new Timer(durationInput, startButton, pauseButton, {
+    onStart(totalDuration) {
+        duration = totalDuration
+    },
+    onTick(timerRemaining) {
+        circle.setAttribute('stroke-dashoffset', 
+            perimeter * timerRemaining / duration - perimeter
+        )
+    },
+    onComplete() {
+        console.log('Time is complete')
+    }
+})
+
